07bc17e45ef3606f1ead20c8ce4610b7aa3216c9
andre <cancino_12_21@outlook.es>
2021-02-04 17:54:30 -0500

    version 1

---
+++ ciclo_for.py
@@ -0,0 +1,5 @@
+numero = input("Ingrese un número:")
+for cifra in numero:
+    if cifra == "6":
+        break
+    print(int(cifra))
---
+++ ciclo_for_2.py
@@ -0,0 +1,6 @@
+def run():
+    for numero in range (100):
+        if numero>50:
+            continue
+        print(numero)
+run()
